b60aa494a25bf51c7f207788f6bc2efb7f38406b
victor nwaiwu <victor.nwaiwu@andela.com>
2017-02-12 15:55:36 +0100

    feature(routes): create authentication routes
    create login,  signup and logout routes and using jwt
    
    [Start #139098211]

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,13 @@
 import users from '../controllers/api/users';
 
 const routes = (router) => {
-  router
-    .route('/users')
-      .get(users.findAll);
+  router.post('/users/login', users.login);
+  router.post('/users/signup', users.create);
+  router.post('/users/logout', users.logout);
+  router.get('/users', users.findAll);
+  router.get('/users/:id', users.findOne);
+  router.put('/users/:id', users.updateOne);
+  router.delete('/users/:id', users.remove);
 };
 
 
