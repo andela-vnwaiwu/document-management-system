7b92b6f8d894f02281014975f325c07f39d953d1
victor nwaiwu <victor.nwaiwu@andela.com>
2017-02-14 01:00:29 +0100

    feature(models): edit migration file and auto populate roles table

---
+++ server/db/migrations/20170204170507-create-document.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('Documents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      OwnerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      access: {
+        defaultValue: 'public',
+        type: Sequelize.STRING
+      },
+      CategoryId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Documents');
+  }
+};
