12566cb71fe2071a72e9f20ff1b14b786adf778f
victor nwaiwu <victor.nwaiwu@andela.com>
2017-02-21 13:18:58 +0100

    feature(search): write search controller and test suite

--- server/test/helpers/documents.helper.js
+++ server/test/helpers/documents.helper.js
@@ -25,6 +25,26 @@ const documents = {
   badDoc: {
     title: faker.lorem.words(),
     tags: ['news', 'nature', 'photography']
+  },
+
+  searchPublicDoc: {
+    title: 'cooking',
+    content: `cooking or cookery is the art, technology and craft of preparing 
+      food for consumption with the use of heat. Cooking techniques and ingredients 
+      vary widely across the world, from grilling food over an open fire to using 
+      electric stoves, to baking in various types of ovens, reflecting     
+      unique environmental, economic, and cultural traditions and trends.`,
+    tags: ['household', 'chef', 'cooking', 'andela']
+  },
+
+  searchPrivateDoc: {
+    title: 'Software',
+    content: `Computer software includes computer programs, libraries and 
+      related non-executable data, such as online documentation or digital media.
+      Computer hardware and software require each other and neither can be 
+      realistically used on its own.`,
+    isPublic: false,
+    tags: ['programming', 'code', 'software', 'andela']
   }
 };
 
